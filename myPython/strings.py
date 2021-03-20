507d6f91889588563ea4ef3781d7a5d7b521163c
1frontier <palikhelsanjeeb@gmail.com>
2021-03-20 14:38:55 +0100

    string chapter

---
+++ myPython/strings.py
@@ -0,0 +1,12 @@
+print("wahh yo ho ni tarika")
+print("siknai parne kuro")
+print("kei naya garam, kei ramro garam")
+print("Namaste" + "Sir")
+greet = "Namaste"
+
+name  = "Palikhel"
+print(greet, name)
+print(greet + name)
+
+name = input("tero naam k ho re, muji")
+print(greet, name)
