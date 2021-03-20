ce8d4bd4243b8d97da1e6f4d98d751df510ee828
1frontier <palikhelsanjeeb@gmail.com>
2021-03-20 14:33:25 +0100

    new chapter

---
+++ myPython/escapechar.py
@@ -0,0 +1,13 @@
+splitString = "Split string \n spiit \n asd \n sad "
+print(splitString)
+print(splitString, "\t qweqwe \t tabbed \t tabbed \t tabbed")
+print(splitString + \
+" \t jksadfj ")
+
+age = 0
+print(type(age))
+print(age)
+name = "22 man"
+print(type(name))
+
+
