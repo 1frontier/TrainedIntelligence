09e92df80bc4c92139ff53e2d6016422543e07d0
1frontier <palikhelsanjeeb@gmail.com>
2021-04-16 16:16:51 +0200

    next

---
+++ myPython/flowcontrol/if_stmt.py
@@ -0,0 +1,9 @@
+name = input("tero naam k ho")
+age  = int (input ("kati umer vo {0} ?".format(name)))
+
+if age >= 18 :
+  print ("vote halna pais")
+  print ("bihe ni handey hunsa")
+else : 
+  print("bacchai resa")
+  print ("naabalig hos, kurr {0} barsa".format(18-age))
